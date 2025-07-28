7b9949ad1bd3bcd1f1ae771ad55a9e7633579bce
Your Name <jaymercy510@gmail.com>
2025-07-28 14:22:44 -0700

    updtaed js and css

--- Project2/index.js
+++ Project2/index.js
@@ -22,7 +22,7 @@ function rollDice() {
     }
     
 
-    diceResult.textContent = `dice: ${values.join(', ')}`;
+    // diceResult.textContent = `dice: ${values.join(', ')}`;
     diceImages.innerHTML = images.join('');
     diceImages.classList.add("shake");
 
@@ -34,14 +34,9 @@ function rollDice() {
 }
 
 const toggleBtn = document.getElementById("themeToggle");
-// const icon = document.getElementById("themeIcon");
 
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
-    // if (document.body.classList.contains("dark")) {
-    //     icon.src = "images/toggle.svg";
-    // }else {
-    //     icon.src = ""
-    // }
+  
 });
