481f5f38ff0daf303ffef057ad724c4e649fefe5
Your Name <jaymercy510@gmail.com>
2025-07-28 09:39:58 -0700

    Dice roller project

--- Project2/index.js
+++ Project2/index.js
@@ -5,14 +5,43 @@ function rollDice() {
     const numOfDice = document.getElementById("numOfDice").value;
     const diceResult = document.getElementById("diceResult");
     const diceImages = document.getElementById("diceImages");
+    
     const values = [];
     const images = [];
 
-    for(let i = 0; i < numOfDice; i++) {
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src="images/${value}.svg">`);    
-    }
+    const diceSound = new Audio('sounds/dicesound.mp3');
+    diceSound.play();
 
-    diceResult.textContent = `dice: ${values.join(', ')}`
+    for(let i = 0; i < numOfDice; i++) {
+         const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+
+        const imageSrc = `images/dice${values}.svg`;
+        images.push(`<img class="dice" src="images/dice${value}.svg" alt="Dice ${value}">`);  
+      
+    }
+    
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+    diceImages.classList.add("shake");
+
+    setTimeout(() => {
+        diceImages.classList.remove("shake");
+     }, 600);
+
+    
 }
+
+const toggleBtn = document.getElementById("themeToggle");
+// const icon = document.getElementById("themeIcon");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // if (document.body.classList.contains("dark")) {
+    //     icon.src = "images/toggle.svg";
+    // }else {
+    //     icon.src = ""
+    // }
+});
